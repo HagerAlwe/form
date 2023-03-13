a04b36063feb508b2bfe367e5b290bb59bdcb9e7
HagerAlwe <125517587+HagerAlwe@users.noreply.github.com>
2023-03-13 23:19:33 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.alert("1-The first letter of the name must be uppercase") ;
+window.alert("2- The first letter of the password must be a capital");
+function nots() {
+  let mat =document.getElementById("main");
+
+  if (mat.value[0] == mat.value[0].toUpperCase()){
+
+    document.write("true")
+  }else{
+    document.write("false")
+  }
+}
+
+
+
+
+function nots() {
+  let m =document.getElementById("post");
+
+  if (m.value[0] == m.value[0].toUpperCase()){
+    document.write("false")
+
+  }else{
+    document.write("true")
+  }
+}
+
+
+
+
+
+
